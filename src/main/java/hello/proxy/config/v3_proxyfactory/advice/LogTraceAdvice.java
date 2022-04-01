d3fc2ceebf91980d09b631526cc65fe2701d27dd
mj0144 <minjeong0144@gmail.com>
2022-04-02 00:45:03 +0900

    1. 어드바이저 여러개를 실제 v1,v2에 적용.

---
+++ src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
@@ -0,0 +1,42 @@
+package hello.proxy.config.v3_proxyfactory.advice;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.lang.reflect.Method;
+
+public class LogTraceAdvice implements MethodInterceptor {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAdvice(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        TraceStatus status = null;
+        try {
+
+            Method method = invocation.getMethod();
+
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+
+            //status = logTrace.begin("OrderController.request()"); // 직접개발할때.
+            status = logTrace.begin(message); // 동적으로 사용
+
+            //target호출
+            //Object result = method.invoke(target, args);// 실제객체 호출
+            Object result = invocation.proceed();
+
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
